db484eed85457a555262aeccbe1a0a449be5e998
Kez <58428891+kezdev@users.noreply.github.com>
2025-02-02 21:03:32 +0000

    styling

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -1,12 +1,22 @@
-export default function Layout({ children }) {
+export default function Layout({ children, count }) {
     return (
-        <div>
-            <nav className={'absolute top-0 left-0 w-full p-4 bg-white shadow-md bg-opacity-25'}>
-                <h1 className="text-center text-2xl font-bold">Tiny URL</h1>
+        <div className={'p-[100px]'}>
+            <nav>
+                <h1 className="text-center text-5xl font-bold pb-[10px] opacity-50">Tiny URL</h1>
+                <h3 className={'text-center text-xl pb-[50px] opacity-50'}>Shorten any URL with ease</h3>
             </nav>
-            <main className="min-h-screen flex items-center justify-center">
+            <main className="flex justify-center">
                 {children}
             </main>
+            <footer className="absolute bottom-0 left-0 w-full">
+                <div className="container mx-auto my-8 flex justify-between items-center">
+                    <a href={'https://kezhall.dev'} target={'_blank'} className="flex items-center">
+                        <p className="text-center text-black opacity-50">Kez Hall</p>
+                    </a>
+
+                    <p className="text-center text-black opacity-50">Last updated Feb, 2025.</p>
+                </div>
+            </footer>
         </div>
     );
 }
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import Layout from '@/Layouts/Layout.jsx';
 import { Head } from '@inertiajs/react';
 import { useState } from 'react';
-import { DocumentDuplicateIcon } from '@heroicons/react/24/solid';
+import { DocumentDuplicateIcon, ArrowTopRightOnSquareIcon } from '@heroicons/react/24/solid';
 
 export default function Home({ count }) {
     const [data, setData] = useState({ url: '' });
@@ -42,7 +42,7 @@ export default function Home({ count }) {
         } finally {
             setProcessing(false);
         }
-    };
+    }
 
     const copyToClipboard = () => {
         if (shortUrl) {
@@ -51,33 +51,37 @@ export default function Home({ count }) {
                 setTimeout(() => setCopied(false), 2000);
             });
         }
-    };
+    }
+
+    const navigateToShortUrl = () => {
+        if (shortUrl) {
+            window.open(shortUrl, '_blank');
+        }
+    }
 
     return (
-        <Layout>
+        <Layout count={count}>
             <Head title="Create short links" />
 
             <div>
                 <div className="mx-auto py-[50px] px-[50px] w-[600px] text-center bg-white bg-opacity-25 rounded-lg shadow-md">
-                    <h2 className="text-xl font-semibold text-center mb-2">Enter a URL to shorten</h2>
-                    <p className="text-center text-black mb-4">Total URLs shortened: {count}</p>
-                    <form onSubmit={handleSubmit} className="space-y-4 w-full text-center">
+                    <form onSubmit={handleSubmit} className="w-full flex items-center">
                         <input
                             autoFocus={true}
                             type="url"
                             name="url"
                             id="url"
-                            placeholder="Enter URL"
+                            placeholder="Enter your URL"
                             value={data.url}
                             onChange={(e) => setData({ ...data, url: e.target.value })}
                             required
-                            className="w-full p-2 border-0 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-center bg-white bg-opacity-25"
+                            className="w-full p-2 border-0 rounded-md focus:outline-none text-center bg-white bg-opacity-25"
                         />
                         {errors.url && <p className="text-red-500 text-sm">{errors.url}</p>}
                         <button
                             type="submit"
                             disabled={processing}
-                            className="px-5 bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition disabled:opacity-50"
+                            className="ml-4 px-5 bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition disabled:opacity-50"
                         >
                             {processing ? 'Shortening...' : 'Shorten'}
                         </button>
@@ -85,17 +89,23 @@ export default function Home({ count }) {
                 </div>
 
                 {shortUrl && (
-                    <div className="mx-auto py-[30px] px-[50px] mt-10 bg-white rounded-lg shadow-md flex items-center space-x-4 bg-opacity-25">
-                        <p>Your short URL</p>
-                        <a href={shortUrl} target="_blank" rel="noopener noreferrer" className=" flex-1 text-blue-500 bg-gray-50 bg-opacity-25 px-5 py-2 rounded-md break-all truncate">
-                            {shortUrl}
-                        </a>
+                    <div
+                        className="mx-auto py-[30px] px-[50px] mt-10 bg-white rounded-lg shadow-md flex items-center space-x-4 bg-opacity-25">
                         <button
                             onClick={copyToClipboard}
-                            className="p-2 bg-gray-200 rounded-md hover:bg-gray-300 transition"
+                            className="p-2 bg-gray-200 rounded-md hover:bg-gray-300 transition bg-opacity-25"
                         >
                             <DocumentDuplicateIcon className="w-5 h-5 text-gray-600"/>
                         </button>
+                        <button
+                            onClick={navigateToShortUrl}
+                            className="p-2 bg-gray-200 rounded-md hover:bg-gray-300 transition bg-opacity-25"
+                        >
+                            <ArrowTopRightOnSquareIcon className="w-5 h-5 text-gray-600"/>
+                        </button>
+                        <p className=" flex-1 bg-gray-50 bg-opacity-25 px-5 py-2 rounded-md break-all truncate text-center">
+                            {shortUrl}
+                        </p>
                     </div>
                 )}
             </div>
