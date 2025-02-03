a77136fb6d15a16446e570a1ef2d554835419696
Kez <58428891+kezdev@users.noreply.github.com>
2025-02-03 10:52:09 +0000

    Create ShortUrlFactory.php

---
+++ database/factories/ShortUrlFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ShortUrl>
+ */
+class ShortUrlFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'original_url' => $this->faker->url,
+            'short_code' => Str::random(6),
+        ];
+    }
+}
